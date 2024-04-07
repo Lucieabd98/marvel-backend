9ad83cbdc85229b5637607eb40ff8fcf2c450667
Lucieabd98 <l.abadia@orange.fr>
2024-04-07 21:02:31 +0200

    added user

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+//Sign Up routes
+const signUpRoutes = require("./routes/user");
+app.use(signUpRoutes);
 
 // Comics routes
 const comicsRoutes = require("./routes/comics");
